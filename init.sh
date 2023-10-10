47f714f3d0d6ded1f2c7fa530cbcb5aa42ea1931
ducval01 <valentine.duclos.1@ulaval.ca>
2023-10-10 19:10:13 -0400

    changement

---
+++ init.sh
@@ -0,0 +1,7 @@
+
+mkdir /home/etudiant8/EtudesCas/EC1_etudiant08/data
+
+mkdir /home/etudiant8/EtudesCas/EC1_etudiant08/data/raw_data
+
+mkdir /home/etudiant8/EtudesCas/EC1_etudiant08/data/reference
+

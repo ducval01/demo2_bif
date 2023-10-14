37f89f980ecc72c047c23ba5c7c227277f307c35
ducval01 <valentine.duclos.1@ulaval.ca>
2023-10-14 12:06:27 -0400

    script partie 1

---
+++ EtudeCas/EC1_etudiant08/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir /home/etudiant8/EtudeCas/EC1_etudiant08/data
+mkdir /home/etudiant8/EtudeCas/EC1_etudiant08/analyses
+mkdir /home/etudiant8/EtudeCas/EC1_etudiant08/resultats
+
+
+
+cd /home/etudiant8/EtudeCas/EC1_etudiant08/data 
+ln -sf /home/public/EtudeCas1/raw_data/
+ln -sf /home/public/rna_seq/reference/
+
+mkdir /home/etudiant8/EtudeCas/EC1_etudiant08/data/adaptors 
+
+cp /home/public/rna_seq/demo/adaptors.fa /home/etudiant8/EtudeCas/EC1_etudiant08/data/adaptors/adaptors.fa
+
+
+touch README.md

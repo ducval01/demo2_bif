a94e050c30c3ece16651af1d403c61ea86a119bf
ducval01 <valentine.duclos.1@ulaval.ca>
2023-10-18 22:52:46 -0400

    retire le init.sh en trop

--- init.sh
+++
@@ -1,7 +0,0 @@
-
-mkdir /home/etudiant8/EtudesCas/EC1_etudiant08/data
-
-mkdir /home/etudiant8/EtudesCas/EC1_etudiant08/data/raw_data
-
-mkdir /home/etudiant8/EtudesCas/EC1_etudiant08/data/reference
-
